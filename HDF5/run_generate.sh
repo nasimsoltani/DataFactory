c2b189f6dc07f91601d7b89ab5f11c8336a3f551
Debashri Roy <debashri.roy.08@gmail.com>
2021-12-11 19:54:29 -0500

    Create run_generate.sh

---
+++ HDF5/run_generate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# ----------------------------------------------------
+python -u ./Generate_HDF5_Dataset.py \
+--destination_dir PATH_TO_DESTINITION_DIR \
+--metadata_file_path PATH_TO_METADATA_FILE \
+--source_dir PATH_TO_SOURCE_DIR \
+--hdf5_file_name HDF_FILE_NAME
